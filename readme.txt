c6892db1a9456bae66980944c583e33178ef16d2
AndreySchmidt <ringnull@gmail.com>
2023-09-06 20:57:23 +0300

    autocomplete discover hint

---
+++ readme.txt
@@ -0,0 +1,1 @@
+npm i @tarekraafat/autocomplete.js
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,33 @@
-import hello from "./modules/hello";
+// import hello from "./modules/hello";
 
-import AirDatepicker from "air-datepicker";
-import "air-datepicker/air-datepicker.css";
+// import AirDatepicker from "air-datepicker";
+// import "air-datepicker/air-datepicker.css";
 
-new AirDatepicker("#datepicker");
+// new AirDatepicker("#datepicker");
 
-console.log(hello);
+// console.log(hello);
+
+import autoComplete from "@tarekraafat/autocomplete.js";
+
+const autoCompleteJS = new autoComplete({
+  selector: "#country",
+  placeholder: "Here is a placeholder",
+  data: {
+    src: ["country1", "country2", "country3"],
+  },
+  resultItem: {
+    highlight: true,
+  },
+  resultsList: {
+    maxResults: 10,
+  },
+  events: {
+    input: {
+      selection: (event) => {
+        const selection = event.detail.selection.value;
+        autoCompleteJS.input.value = selection;
+      },
+    },
+  },
+  searchEngine: "strict", // strict | loose
+});
